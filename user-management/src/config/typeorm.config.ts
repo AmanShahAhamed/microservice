e40892fee4f4f7ab0d15e4e946f92ddfcd4faec8
Aman Shah Ahamed <amanshah.a@kgs.live>
2025-05-20 18:19:53 +0530

    first commit

---
+++ user-management/src/config/typeorm.config.ts
@@ -0,0 +1,25 @@
+import { ConfigService } from '@nestjs/config';
+import {
+  TypeOrmModuleAsyncOptions,
+  TypeOrmModuleOptions,
+} from '@nestjs/typeorm';
+
+export const typeOrmConfig: TypeOrmModuleAsyncOptions = {
+  inject: [ConfigService],
+  useFactory: async (
+    configService: ConfigService,
+  ): Promise<TypeOrmModuleOptions> => ({
+    type: 'postgres',
+    host: configService.getOrThrow<string>('DB_HOST'),
+    port: configService.getOrThrow<number>('DB_PORT'),
+    username: configService.getOrThrow<string>('DB_USER'),
+    password: configService.getOrThrow<string>('DB_PASS'),
+    database: configService.getOrThrow<string>('DB_NAME'),
+    entities: ['dist/*/**/***.entity{.ts,.js}'],
+    migrations: ['dist/*/**/***.migration{.ts,.js}'],
+    synchronize: false,
+    autoLoadEntities: true,
+    // logger: 'simple-console',
+    // logging: true,
+  }),
+};
