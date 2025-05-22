efbffc40fe8f5784136da76409cf9953b2954d2c
Aman Shah Ahamed <amanshah.a@kgs.live>
2025-05-22 10:58:37 +0530

    docker file improved

--- api-gateway/src/app.service.ts
+++ api-gateway/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! gate way4';
+    return 'Hello World from app gate way';
   }
 }
--- order/src/main.ts
+++ order/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT ?? 3002;
+  Logger.log(`app is running on ${port}`);
+  await app.listen(port);
 }
 bootstrap();
